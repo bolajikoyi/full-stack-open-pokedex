1649164664d29ef14302fc9420f3d10beda9635b
Omobolaji Koyi <omobolaji-oluwajuwon.koyi@deutschebahn.com>
2022-02-24 00:36:59 +0100

    deploy

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+/* eslint-disable semi */
+/* eslint-disable no-undef */
 const express = require("express");
 const app = express();
 
@@ -5,6 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+app.get("/version", (req, res) => {
+  res.send("1"); // change this string to ensure a new version deployed
+});
 
 app.listen(PORT, () => {
   console.log("server started on port 5000");
